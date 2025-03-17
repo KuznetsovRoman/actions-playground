55e5939b1c72dda587c6e6fa2461f2d02302797e
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2025-03-17 04:29:02 +0300

    Update example.testplane.ts

--- testplane-tests/example.testplane.ts
+++ testplane-tests/example.testplane.ts
@@ -3,6 +3,6 @@ describe("test", () => {
     it("example", async ({browser}) => {
         await browser.url("https://example.com/");
 
-        await expect(browser.$("p")).toHaveTextContaining("This domain is for use in illustrative examples in documents");
+        await expect(browser.$("p")).toHaveTextContaining("This domain is not for use in illustrative examples in documents");
     });
 });
